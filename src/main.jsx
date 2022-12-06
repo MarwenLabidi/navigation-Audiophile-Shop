4824c6f7b26b7653f42908c7924db36a1bb60cfc
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-06 10:09:07 +0100

    🚧 Work in progress : setting module federation plugin

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const mount = (el) => {
+        ReactDOM.createRoot(el).render(
+                <React.StrictMode>
+                        <App />
+                </React.StrictMode>
+        );
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+if (!window.containerContext) {
+        mount(document.getElementById("root"));
+}
+
+export { mount };
