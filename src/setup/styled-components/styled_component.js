274b5f60b7d5156e6e801e4e970d9f5f08c65bbf
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-07 19:16:50 +0100

    💄 style app

--- src/setup/styled-components/styled_component.js
+++ src/setup/styled-components/styled_component.js
@@ -1,30 +1,57 @@
 import styled, { css } from "styled-components";
 
 export const StyledLinksApp = styled.section`
-        background-color: green;
-	margin-top: 20px;
-	& .cardOne{
-		background-color: red;
-		width: 200px;
-		height:200px;
-		margin:auto;
-	}
-        & .shop_section {
-                display: flex;
-                align-items: center;
-                gap: 10px;
-                object-fit: contain;
+        padding: 10px;
+        & .cardOne {
+		position:relative;
+                /* background-color: red; */
+                width: 300px;
+                height: 200px;
+                margin: 0 auto;
+                /* padding: 10px; */
+                margin-top: 20px;
+                & .cardOne__img {
+                        background-color: var(--color-gray-300);
+			border-radius:10px;
+			position:relative;
+			top:50px;
+
+                        width: 100%;
+                        height: 150px;
+                        & img {
+                                width: 120px;
+                                height: 120px;
+                                object-fit: contain;
+                                margin: auto;
+				position:relative;
+				top:-50px;
+                        }
+                }
+                & .cardOne__container {
+			position:relative;
+			z-index: 111111;
+			top:-20px;
+                        & .cardOne__container__text {
+				text-align:center;
+				
+                        }
+                        & .shop_section {
+                                display: flex;
+                                align-items: center;
+                                gap: 10px;
+                                object-fit: contain;
+				justify-content:center
+                        }
+                }
+		&:hover{
+			cursor:pointer;
+		}
         }
-        & img {
-                width: 150px;
-                height: 150px;
-		object-fit: contain;
-        }
+
         ${({ isBrowser }) =>
                 isBrowser &&
                 css`
                         display: flex;
-			justify-content: center;
-			gap: 30px;
+                        justify-content: center;
                 `}
 `;
